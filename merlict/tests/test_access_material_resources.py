e2a628fde2097cdde1db90ee9d6e9fab58217208
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-09-28 14:18:04 +0200

    update materials

---
+++ merlict/tests/test_access_material_resources.py
@@ -0,0 +1,25 @@
+import merlict
+
+
+def test_list_material_surfaces():
+    keys = merlict.materials.surfaces.list_resources()
+    assert len(keys) > 0
+
+
+def test_list_material_media():
+    keys = merlict.materials.media.list_resources()
+    assert len(keys) > 0
+
+
+def test_read_material_surfaces():
+    surf = merlict.materials.surfaces.init(key="perfect_absorber")
+    assert "material" in surf
+    assert "specular_reflection" in surf
+    assert "diffuse_reflection" in surf
+    assert "color" in surf
+
+
+def test_read_material_media():
+    medi = merlict.materials.media.init(key="water_T293K")
+    assert "refraction" in medi
+    assert "absorbtion" in medi
