f9ad96dbaaa66cabe73e4cda009d272e454ca976
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-02-04 14:46:26 +0100

    check spectra

--- merlict/materials/spectra/__init__.py
+++ merlict/materials/spectra/__init__.py
@@ -1,8 +1,21 @@
-def init():
-    return {
-        "name": "",
-        "x": [],
-        "y": [],
-        "x_name": "",
-        "y_name": "",
-    }
+from ... import utils
+
+
+def assert_spectrum_is_valid(spectrum, ymin=None, ymax=None):
+    assert len(spectrum["x"]) >= 2
+    assert len(spectrum["x"]) == len(spectrum["y"])
+
+    assert utils.is_free_of_nan(spectrum["x"])
+    assert utils.is_all_greater_zero(spectrum["x"])
+    assert utils.is_monotonically_increasing(spectrum["x"])
+
+    assert utils.is_free_of_nan(spectrum["y"])
+
+    if ymin is not None:
+        assert np.all(spectrum["y"] >= ymin)
+
+    if ymax is not None:
+        assert np.all(spectrum["y"] <= ymax)
+
+    assert "\n" not in spectrum["x_label"]
+    assert "\n" not in spectrum["y_label"]
